d539dbae5b7cb80a244135f35abc00cc4fe06167
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2023-01-23 16:47:10 +0000

    Add routes

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+const cubeController = require('./controllers/cubeController.js');
+const router = require('express').Router();
+
+router.get('/', (req, res) =>{
+    res.render('index');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// app.get('/create', (req, res) => {
+//     res.render('create');
+// });
+
+router.get('/create', cubeController.getCreateCube);
+
+module.exports = router;
