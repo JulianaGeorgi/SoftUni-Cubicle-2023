0a1368fe26f4f5a0864b2f583f3b87ac2c924f45
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2023-02-07 11:38:05 +0000

    Added cube model

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,21 +1,30 @@
-const fs = require('fs');
-const db = require('../db.json');
-const path = require('path');
+const mongoose = require('mongoose');
 
-class Cube {
-    constructor(name, description, imageUrl, difficultyLevel) {
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.difficultyLevel = difficultyLevel;
-    }
+// defining the schema and some basic validators
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 50,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
 
-    save() {
-        this.id = db.cubes[db.cubes.length - 1].id + 1;
-        db.cubes.push(this);
-        const jsonData = JSON.stringify(db, null, 2);
-        fs.writeFileSync(path.resolve(__dirname, '../db.json'), jsonData);
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        max: 6,
+        min: 1,
     }
-}
+});
+
+const Cube = mongoose.model('Cube', cubeSchema); // creating the model, 
+// passing the name and what is the schema (in the db -> the name will be with small letters and plural)
 
 module.exports = Cube;
