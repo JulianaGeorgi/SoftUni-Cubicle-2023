089b915d0758c4cfa5210b3b2e46563cfbb1b5b0
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2023-02-08 12:13:19 +0000

    Fixed paths

--- src/routes.js
+++ src/routes.js
@@ -8,13 +8,12 @@ router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 router.get('/404', homeController.getErrorPage);
 
-router.get('/create', cubeController.getCreateCube);
-router.post('/create', cubeController.postCreateCube);
+router.get('/cubes/create', cubeController.getCreateCube);
+router.post('/cubes/create', cubeController.postCreateCube);
 router.get('/cubes/:cubeId/details', cubeController.getDetails);
-
 router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory);
 router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
 
-router.use('/accessory', accessoryController); // if the url starts with this, use that controller
+router.use('/accessories', accessoryController); // if the url starts with this, use that controller
 
 module.exports = router;
