3fc5c270223bad49ca7cfb3c6297529a6d30905e
JulianaGeorgi <dzhuliyana.georgieva@gmail.com>
2023-02-11 17:24:55 +0000

    Register user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,21 +1,31 @@
 const router = require('express').Router();
 
-router.get('/login', (req, res)=>{
+const authService = require('../services/authServices');
+
+router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.get('/register', (req, res)=>{
+router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', (req, res)=>{
-    const {username, password, repeatPassword} = req.body;
+router.post('/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
 
-    if(password !== repeatPassword){
+    if (password !== repeatPassword) {
+        return res.redirect('/404');
+    }
+
+    const existingUser = await authService.getUserByUsername(username);
+
+    if (existingUser) {
         return res.status(404).end();
     }
 
-    
+    const user = await authService.register(username, password);
+
+    res.redirect('/login');
 });
 
 module.exports = router;
